edda2a3649a6d61776fd0a7bc90c9956e81fc3fd
pravirchugh <pravirchugh@yahoo.com>
2023-01-29 00:40:33 -0800

    Patients can only upvote/downvote a provider once

--- src/components/Patients.js
+++ src/components/Patients.js
@@ -10,41 +10,79 @@ import {
 function Patients(props) {
     
     function increaseCount(index) {
-        // console.log(index);
-        let newProviders = [...props.providers];
-        // console.log(newProviders[index][2]);
-        newProviders[index][2] = newProviders[index][2] + 1;
-        props.setProviders(newProviders);
+        if(!props.providers[index][6]){
+            // console.log(index);
+            let newProviders = [...props.providers];
+            // console.log(newProviders[index][2]);
+            newProviders[index][2] = newProviders[index][2] + 1;
+            newProviders[index][6] = !newProviders[index][6];
+            props.setProviders(newProviders);
+            
+        } else {
+            let newProviders = [...props.providers];
+            // console.log(newProviders[index][2]);
+            newProviders[index][2] = newProviders[index][2] - 1;
+            newProviders[index][6] = !newProviders[index][6];
+            props.setProviders(newProviders);
+            
+        }
     }
 
     function increaseEquitable(index) {
-        // console.log(index);
-        let newProviders = [...props.providers];
-        // console.log(newProviders[index][2]);
-        newProviders[index][3] = Math.min(newProviders[index][3] + 1, newProviders[index][2]);
-        props.setProviders(newProviders);
+        if(!props.providers[index][7]){
+            // console.log(index);
+            let newProviders = [...props.providers];
+            // console.log(newProviders[index][2]);
+            newProviders[index][3] = Math.min(newProviders[index][3] + 1, newProviders[index][2]);
+            newProviders[index][7] = !newProviders[index][7];
+            props.setProviders(newProviders);
+        } else {
+            let newProviders = [...props.providers];
+            // console.log(newProviders[index][2]);
+            newProviders[index][3] = newProviders[index][3] - 1;
+            newProviders[index][7] = !newProviders[index][7];
+            props.setProviders(newProviders);
+            
+        }
     }
 
     function increaseKnowledge(index) {
-        // console.log(index);
-        let newProviders = [...props.providers];
-        // console.log(newProviders[index][2]);
-        newProviders[index][4] = Math.min(newProviders[index][4] + 1, newProviders[index][2]);
-        props.setProviders(newProviders);
+        if(!props.providers[index][8]){
+            // console.log(index);
+            let newProviders = [...props.providers];
+            // console.log(newProviders[index][2]);
+            newProviders[index][4] = Math.min(newProviders[index][4] + 1, newProviders[index][2]);
+            newProviders[index][8] = !newProviders[index][8];
+            props.setProviders(newProviders);
+        } else {
+            let newProviders = [...props.providers];
+            // console.log(newProviders[index][2]);
+            newProviders[index][4] = newProviders[index][4] - 1;
+            newProviders[index][8] = !newProviders[index][8];
+            props.setProviders(newProviders);
+        }
     }
 
     function increaseCompassion(index) {
-        // console.log(index);
-        let newProviders = [...props.providers];
-        // console.log(newProviders[index][2]);
-        newProviders[index][5] = Math.min(newProviders[index][5] + 1, newProviders[index][2]);
-        props.setProviders(newProviders);
+        if(!props.providers[index][9]){
+            // console.log(index);
+            let newProviders = [...props.providers];
+            // console.log(newProviders[index][2]);
+            newProviders[index][5] = Math.min(newProviders[index][5] + 1, newProviders[index][2]);
+            props.setProviders(newProviders);
+        } else {
+            let newProviders = [...props.providers];
+            // console.log(newProviders[index][2]);
+            newProviders[index][5] = newProviders[index][5] - 1;
+            newProviders[index][9] = !newProviders[index][9];
+            props.setProviders(newProviders);
+        }
     }
 
     function registerNewProvider(txt) {
         let newProviders = [...props.providers];
         console.log(txt);
-        let lastArray = [props.providers.length, txt, 0, 0, 0, 0];
+        let lastArray = [props.providers.length, txt, 0, 0, 0, 0, false, false, false, false];
         newProviders.push(lastArray);
         props.setProviders(newProviders);
     }
